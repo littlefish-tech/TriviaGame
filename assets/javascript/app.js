e2acab009c50c9df1dba24083a11f8f6ad6e1047
Yimeng <yimeng.yu@gmail.com>
2019-10-24 11:22:14 -0700

    Pseudocode steps for the game

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+// when user click the start button
+// The timer starts to run
+// user start to click the question
+//when click the right answer, then win++
+//when click the wrong answer, then loss++
+// For not clicked answer, unclicked++
