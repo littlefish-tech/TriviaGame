f8b18c9a92f5466455de3a78eecc6b6c0e24e81b
Yimeng <yimeng.yu@gmail.com>
2019-10-25 00:03:10 -0700

    update HTML and JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,3 +4,50 @@
 //when click the right answer, then win++
 //when click the wrong answer, then loss++
 // For not clicked answer, unclicked++
+// when times up, finish and show the result
+
+var displayTime = 60;
+var questionArr = [
+  {
+    question: "What is the first letter of the alphabet?",
+    answers: ["a", "b", "z", "e"],
+    correct: "a"
+  },
+  {
+    question: "What is the last letter of the alphabet?",
+    answers: ["a", "b", "z", "e"],
+    correct: "z"
+  },
+  {
+    question: "What is the second letter of the alphabet?",
+    answers: ["a", "b", "z", "e"],
+    correct: "b"
+  }
+];
+
+$("#gameContainer").hide();
+
+$("#startButton").on("click", function() {
+  console.log("button clicked");
+  $("#startButton").hide();
+  $("#gameContainer").show();
+  $("#timeRemaining").text(displayTime);
+});
+
+// setTimeout(function() {
+//   console.log("set time out ran after 3second");
+// }, 3000);
+
+setInterval(function() {
+  console.log("set interval ran after 1 second");
+  displayTime--;
+  $("#timeRemaining").text(displayTime);
+}, 1000);
+
+for (var i = 0; i < questionArr.length; i++) {
+  var currentQuestion = questionArr[i].question;
+  $("#questionsDisplay").append(currentQuestion);
+
+  var radioBtn = $('<input type="radio" name="rbtnCount" />');
+  radioBtn.appendTo("#questionDisplay");
+}
